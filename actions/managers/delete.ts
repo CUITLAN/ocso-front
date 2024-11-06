6f4acb13f57786ad4bcfef24d992362d5e62fb7a
Cuitlan <alan78707@gmail.com>
2024-11-05 23:42:49 -0600

    Manager cambied for locations

---
+++ actions/managers/delete.ts
@@ -0,0 +1,25 @@
+"use server";
+
+import { API_URL } from "@/constants";
+import authHeaders from "@/helpers/Auth.headers";
+import { revalidateTag } from "next/cache";
+
+export default async function createManagers(managerId: string, formData: FormData){
+
+let manager: any = {}
+
+    for (const key of formData.keys()) {
+        const value = formData.get(key);
+        manager[key] = formData.get(key);
+
+        
+    }
+    const res = await fetch(`${API_URL}/managers/${managerId}`,{
+        method: "DELETE",
+        body: JSON.stringify(manager),
+        headers: {...authHeaders()}
+    })
+    console.log(res.status);
+        revalidateTag("dashbaord:managers")
+    
+}
---
+++ actions/managers/update.ts
@@ -0,0 +1,28 @@
+"use server";
+
+import { API_URL } from "@/constants";
+import authHeaders from "@/helpers/Auth.headers";
+import { revalidateTag } from "next/cache";
+
+export default async function createManagers(managerId: string, formData: FormData){
+
+let manager: any = {}
+
+    for (const key of formData.keys()) {
+        const value = formData.get(key);
+        manager[key] = formData.get(key);
+
+        
+    }
+    const res = await fetch(`${API_URL}/managers/${managerId}`,{
+        method: "PATCH",
+        body: JSON.stringify(manager),
+        headers: {...authHeaders()}
+    })
+    console.log(res.status);
+
+    if(res.status==201){
+        revalidateTag("dashbaord:managers")
+        revalidateTag(`dashbaord:managers:${managerId}`)
+    }
+}
