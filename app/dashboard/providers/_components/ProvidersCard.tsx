c0c0883a676ed7198e384263adc8001691163575
Cuitlan <alan78707@gmail.com>
2024-11-07 21:46:12 -0600

    Modal Proveedor: Implemented

--- app/dashboard/providers/_components/ProvidersCard.tsx
+++ app/dashboard/providers/_components/ProvidersCard.tsx
@@ -4,7 +4,7 @@ import { LuPlus } from "react-icons/lu";
 
 export default function ProvidersCards({ providers }: { providers: Providers }) {
     return (
-        <Card className="">
+        <Card className="w-full min-w-[350px]">
             <CardHeader>
                 <b>{providers.providerName}</b>
             </CardHeader>
@@ -13,8 +13,8 @@ export default function ProvidersCards({ providers }: { providers: Providers }) 
                 <p>Correo: <b>{providers.providerEmail}</b></p>
                 <p>Teléfono: <b>{providers.providerPhoneNumber}</b></p>
                 {
-                    Array.isArray(providers.products) && providers.products.length > 0 ? (
-                        <p>Productos: <b>{providers.products.length}</b> productos </p>
+                    Array.isArray(providers.products) && providers.products.length !== 0 ? (
+                        <p>Tiene:  <b>{providers.products.length}</b> productos </p>
                     ) : (
                         <p>No tiene productos</p>
                     )
--- app/dashboard/providers/page.tsx
+++ app/dashboard/providers/page.tsx
@@ -5,6 +5,8 @@ import ProvidersCards from "./_components/ProvidersCard";
 import Link from "next/link";
 import { LuPlus } from "react-icons/lu";
 import { Button } from "@nextui-org/react";
+import CreateProviders from "./_components/CreateProviders";
+import FormCreateProviders from "./_components/FormCreateProviders";
 export default async function ProviderPage() {
   const res = await fetch(`${API_URL}/providers`, {
     headers: {
@@ -14,9 +16,9 @@ export default async function ProviderPage() {
   const provider: Providers[] = await res.json();
   return (
     <div className="flex flex-grow-0 flex-col h-[90vh] items-end w-full px-10 pt-10">
-        <Button className="w-fit" color="primary">
-            <LuPlus size={20}/>
-        </Button>
+        <CreateProviders>
+          <FormCreateProviders/>
+        </CreateProviders>
       <div className="flex flex-wrap w-full flex-grow-0 gap-14 px-20 h-[90vh] ">
         {provider.map((provider: Providers) => (
           <Link
